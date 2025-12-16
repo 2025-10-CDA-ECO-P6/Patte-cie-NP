4bef469bc2ed3f956c951caeb8a2cd398260f15d
Pierre-MarieMarchio <pierremariemarchio.pro@gmail.com>
2025-12-16 13:51:46 +0100

    fix: fix app mprisma connect

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
-import { PrismaClient } from "@prisma/client";
 import express from "express";
 import coreRoutes from "./core/routes";
-
+import {prisma} from "../lib/prisma"
 
 const app = express();
 
 // Prisma connection
-export const prisma = new PrismaClient();
+
 prisma.$connect();
 
 app.use(express.json());
