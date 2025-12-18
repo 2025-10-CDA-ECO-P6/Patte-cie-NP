ec02f461e20f05de9be6e8ffaabcdd04c77d573e
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2025-12-18 15:14:58 +0100

    refactor: streamline authentication middleware to use cookie for token retrieval

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,12 @@ import { prisma } from "../lib/prisma";
 import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(cookieParser());
 
 // Prisma connection
 prisma.$connect();
 
 app.use(express.json());
-app.use(cookieParser());
 app.use("/api", coreRoutes);
 
 export default app;
--- src/features/auth/middlewares/auth.middleware.ts
+++ src/features/auth/middlewares/auth.middleware.ts
@@ -5,12 +5,7 @@ import jwt from "jsonwebtoken";
 
 export const authenticationMiddleware = () => {
     return async (req: Request, res: Response, next: NextFunction) => {
-        const authHeader = req.headers.authorization;
-
-        if (!authHeader) {
-            return res.status(401).json({ message: "Authorization header missing" });
-        }
-        const token = authHeader.split(" ")[1]; // on split le token du header
+        const token = req.cookies.access_token;
 
         if (!token) {
             return res.status(401).json({ message: "Token missing" });
