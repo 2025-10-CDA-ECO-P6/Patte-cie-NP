0d0ec570f2d3fef21e26bf82632f523dd02d8f69
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2025-12-18 11:56:52 +0100

    feat: add cookie-parser middleware for handling cookies

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import coreRoutes from "./core/routes";
 import { prisma } from "../lib/prisma";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -8,6 +9,7 @@ const app = express();
 prisma.$connect();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api", coreRoutes);
 
 export default app;
