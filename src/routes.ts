4628a79c46b705014455601e4c7fa239009ff4d5
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2026-01-19 12:15:08 +0100

    feat: add authentication routes to the main router

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-
 import coreRoutes from "./core/routes";
 import moduleHealthRecordRoutes from "./features/health-record/routes";
+import authRoutes from "./features/auth/routes";
 
 const router = Router();
 
 router.use(coreRoutes);
 router.use(moduleHealthRecordRoutes);
+router.use(authRoutes);
 
 export default router;
