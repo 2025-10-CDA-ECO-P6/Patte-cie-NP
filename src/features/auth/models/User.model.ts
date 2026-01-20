3396af13cc6c5363c3ee82f645389d564d9cab8b
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2026-01-20 10:25:58 +0100

    feat: add User model with properties and constructor

---
+++ src/features/auth/models/User.model.ts
@@ -0,0 +1,29 @@
+interface UserProps {
+    id: string;
+    email: string;
+    password: string;
+    roleId: string;
+    createdAt: Date;
+    updatedAt?: Date;
+    isDeleted: boolean;
+}
+
+export class User {
+    public readonly id: string;
+    public email: string;
+    public password: string;
+    public roleId: string;
+    public readonly createdAt: Date;
+    public updatedAt?: Date;
+    public isDeleted: boolean;
+
+    constructor(props: UserProps) {
+        this.id = props.id;
+        this.email = props.email;
+        this.password = props.password;
+        this.roleId = props.roleId;
+        this.createdAt = props.createdAt;
+        this.updatedAt = props.updatedAt;
+        this.isDeleted = props.isDeleted;
+    }
+}
